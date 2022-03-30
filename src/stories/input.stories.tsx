1e3891f95834cb7ed4007ae3c2d37ebab3a67b74
romantut1988 <romantut1988@mail.ru>
2022-03-30 12:56:58 +0300

    controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -31,5 +31,37 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
         - actual value: {value} </>;
 }
 
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState("");
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value);
+    }
+
+    return <input value={parentValue} onChange={ onChange } />
+
+
+}
+export const ControlledCheckBox = () => {
+    const [parentValue, setParentValue] = useState(true);
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked);
+    }
+    return <input type="checkbox" checked={parentValue} onChange={ onChange } />
+
+}
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>("2");
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value);
+    }
+    return <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value={"1"}>Minsk</option>
+        <option value={"2"}>Moscow</option>
+        <option value={"3"}>Kiev</option>
+    </select>
+}
 
 export const ControlledInputWithFixedValue = () => <input value={"it-incubator.by"}/>;
