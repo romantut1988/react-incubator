629079aff6b78df64f3a1135aa335f9101841797
romantut1988 <romantut1988@mail.ru>
2022-04-02 02:44:42 +0300

    test and usereducer

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
+import {reducer, TOGGLE_COLLAPSED} from "./reducer";
 
 type AccordionPropsType = {
     titleValue: string
@@ -7,12 +8,14 @@ type AccordionPropsType = {
 function UncontrolledAccordion(props: AccordionPropsType) {
     console.log("UncontrolledAccordion rendering ")
 
-    let [collapsed, setCollapsed] = useState(false);
+    // let [collapsed, setCollapsed] = useState(false);
+    let [state, dispatch] = useReducer(reducer, {collapsed: false});
 
     return <div>
-        <AccordionTitle title={props.titleValue} onClick = { () => { setCollapsed(!collapsed) } }/>
-        { !collapsed && <AccordionBody/>}
-    </div>;
+        <AccordionTitle title={props.titleValue} onClick = { () => {
+            dispatch({type: TOGGLE_COLLAPSED}) } }/>
+        { !state.collapsed && <AccordionBody/>}
+    </div>
 }
 
 type AccordionTitlePropsType = {
