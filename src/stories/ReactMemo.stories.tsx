d87367a813962876a45cdceba56b13596919a327
romantut1988 <romantut1988@mail.ru>
2022-04-02 18:07:00 +0300

    react.memo components

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+
+export default {
+    title: "React.memo demo",
+}
+
+const NewMessagesCounter = (props: { count: number } ) => {
+    return <div>{props.count}</div>
+}
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log("USERS")
+    return <div> {
+        props.users.map((u, i) => <div key={i}>{u}</div>)
+    }</div>
+}
+const Users = React.memo(UsersSecret);
+
+export const Example1 = () => {
+    console.log("Example1");
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(["Dimych", "Valera", "Artem", "Katya"]);
+
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta' + new Date().getTime()];
+        setUsers(newUsers);
+    }
+
+    users.push('Sveta' + new Date().getTime());
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={addUser}>add user</button>
+        <NewMessagesCounter count={counter}/>
+        <Users users={users}/>
+    </>
+}
