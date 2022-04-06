7983fd129c134120a597d1315818f7622c01c380
romantut1988 <romantut1988@mail.ru>
2022-04-06 15:53:40 +0300

    clock analogue

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -6,6 +6,11 @@ export default {
     component: Clock,
 };
 
-export const BaseExample = () => {
-    return <Clock />
+export const BaseAnalogExample = () => {
+    return <Clock mode={"analog"}/>
 };
+
+export const BaseDigitalExample = () => {
+    return <Clock mode={"digital"}/>
+};
+
--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,17 +1,20 @@
 import React, {useEffect, useState} from "react"
+import {DigitalClockView} from "./DigitalClockView";
+import {AnalogClockView} from "./AnalogClockView";
 
 type PropsType = {
+    mode?: "digital" | "analog"
 }
 
-const get2digitsString = (num: number) => num < 10 ? "0" + num : num
+export const get2digitsString = (num: number) => num < 10 ? "0" + num : num
 
 
 export const Clock: React.FC<PropsType> = (props) => {
     const [date, setDate] = useState(new Date())
 
-    useEffect( () => {
+    useEffect(() => {
 
-        const intervalID = setInterval( () => {
+        const intervalID = setInterval(() => {
             setDate(new Date());
         }, 1000);
 
@@ -20,11 +23,23 @@ export const Clock: React.FC<PropsType> = (props) => {
         }
     }, [])
 
+    let view
+
+    switch (props.mode) {
+        case "analog" :
+            view = <AnalogClockView date={date}/>
+            break;
+        case "digital":
+        default:
+            view = <DigitalClockView date={date}/>
+    }
+
     return <div>
-        <span>{get2digitsString(date.getHours())}</span>
-        :
-        <span>{get2digitsString(date.getMinutes())}</span>
-        :
-        <span>{get2digitsString(date.getSeconds())}</span>
+        {view}
     </div>
 }
+
+export type ClockViewPropsType = {
+    date: Date
+}
+
